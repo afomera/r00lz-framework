62fa8c094ea51cd077c8ce0abc9bc1f23181999b
Andrew Fomera <andrew@zerlex.net>
2019-08-01 15:09:30 -0500

    lint

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -37,12 +37,12 @@ module R00lz
   end
 
   def self.to_underscore(s)
-      s.gsub(
-      /([A-Z]+)([A-Z][a-z])/,
-      '\1_\2').gsub(
-      /([a-z\d])([A-Z])/,
-      '\1_\2').downcase
-    end
+    s.gsub(
+    /([A-Z]+)([A-Z][a-z])/,
+    '\1_\2').gsub(
+    /([a-z\d])([A-Z])/,
+    '\1_\2').downcase
+  end
 end
 
 class Object
