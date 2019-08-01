7f3f08169bb2c980c2393e7f3a7b1962d89c877a
Andrew Fomera <andrew@zerlex.net>
2019-08-01 14:36:01 -0500

    section two and some working ahead on section three

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -6,11 +6,41 @@ module R00lz
 
   class App
     def call(env)
+      kl, act = cont_and_act(env) # controller, and action for given env
+      text = kl.new(env).send(act) # make controller send the action passed to get response
       [
         200,
         { 'Content-Type' => 'text/html' },
-        ['Hello from r00lzs!']
+        [text]
       ]
     end
+
+    def cont_and_act(env)
+      _, con, act, after = env["PATH_INFO"].split('/', 4)
+      con = con.capitalize + "Controller"
+      [Object.const_get(con), act]
+    end
+  end
+
+  class Controller
+    attr_reader(:env)
+    def initialize(env)
+      @env = env
+    end
+
+    def self.to_underscore(s)
+      s.gsub(
+      /([A-Z]+)([A-Z][a-z])/,
+      '\1_\2').gsub(
+      /([a-z\d])([A-Z])/,
+      '\1_\2').downcase
+    end
+  end
+end
+
+class Object
+  def self.const_missing(c)
+    require R00lz.to_underscore(c.to_s)
+    Object.const_get(c)
   end
 end
